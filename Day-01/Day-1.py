6e738ae271368be8a24692866b4b7f2bb3f0383d
Subhash <wanabecoderguy@gmail.com>
2025-08-31 11:42:59 +0530

    Add Day-1 to Day-100 Python files

---
+++ Day-01/Day-1.py
@@ -0,0 +1,34 @@
+# print("Welcome to the Band Name Generator.")
+# print("What's name of the city you grew up in?")
+# city = input()
+# print("What's your pet's name?")
+# pet = input()
+# print("Your band name could be " + city + " " + pet)
+
+#=================================================================================================
+
+# print("Hello World! \n Hello World! \n Hello World!")
+
+# print("Hello" + " " + "Angela")
+
+#=================================================================================================
+
+# print("Hello " + input("What is your name?") + "!")
+
+# name = input("What is your name?")
+# print("Hello " + name + "!")
+
+#=================================================================================================
+
+# username = input("What is your name? ")
+# length = len(username)
+# print("Your name has " + str(length) + " characters.")
+
+#=================================================================================================
+
+#============================Project of DAY-1============================
+print("Welcome to the Band Name Generator.")
+CITY_NAME = input("What's name of the city you grew up in?\n")
+PET_NAME = input("What's your pet's name?\n")
+print("Your band name could be " + CITY_NAME + " " + PET_NAME)
+#=======================================================================
